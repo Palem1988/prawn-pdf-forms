0f3093e61d5dd617b540abeafb84ab6447e797e1
Edgar Quintana <edgarquintana984@hotmail.com>
2017-04-03 08:18:29 -0500

    Adds Welcome page to the Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+
   resource :solicitud
-  
-  root 'solicitudes#new'
+
+  root 'solicitudes#welcome'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
